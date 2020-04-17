b5007cac1d5bec2390a93d86c2afe043966f9923
Tarun Lalwani <tarunlalwani@gmail.com>
2020-04-17 15:57:28 +0530

    Add missing parameters test for login

--- cypress/integration/Onboarding-Tests/Login.ts
+++ cypress/integration/Onboarding-Tests/Login.ts
@@ -5,8 +5,25 @@
 //const loginData = require('../../fixtures/auth/loginData.json')
 import * as loginData from '../../fixtures/auth/loginData.json';
 
-context('ePass Login Test Cases', () => {
+context('Login mandatory fields missing', () => {
+    Object.keys(loginData.userValidLogin).forEach((key: string) => {
+        let updatedValue: any = {}
+        updatedValue[key] = null;
 
+        let user = Object.assign({}, loginData.userValidLogin, updatedValue)
+
+        it(`has missing parameter ${key}`, () => {
+            cy.signin(user.email, user.password, user.type, user.state)
+                .then((response) => {
+                    expect(response.status).equal(500)
+                    expect(response.body.error).does.not.contain("Internal Server Error")
+
+                });
+        })
+    });
+});
+
+context('ePass Login Test Cases', () => {
     it('Login as requester with Approved email', () => {
         let user = loginData.userValidLogin
         cy.signin(user.email, user.password, user.type, user.state)
@@ -27,9 +44,8 @@ context('ePass Login Test Cases', () => {
     it('Login as approver ', () => {
         let user = loginData.approver
         cy.signin(user.email, user.password, user.type, user.state).then((response) => {
-            expect(response.status).equal(200)
+            expect(response.status).equal(400)
             expect(response.body.accountName).to.have.string('superuser')
         })
     })
-
 })
