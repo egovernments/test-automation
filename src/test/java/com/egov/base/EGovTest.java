72d664a0d1e02725ae1f81fa9ae2fae2ddeca1c0
krishnendu-eGov <khalder@dealersocket.com>
2021-02-04 17:45:54 +0530

    Expose authToken as global config

--- src/test/java/com/egov/base/EGovTest.java
+++ src/test/java/com/egov/base/EGovTest.java
@@ -28,7 +28,8 @@ import net.minidev.json.JSONValue;
 	"classpath:com/egov/business-services",
 	"classpath:com/egov/common-services",
 	"classpath:com/egov/municipal-services"},
-		tags = {"@Create_PaymentWithValidBillID_01"})
+	tags = {"@reports,@searchMdms,@Searcher,@location,@localization,@userotp,@eGovUser,@accessControl," +
+	"@hrms,@collectionServices,@billingServiceDemand,@pdfservice,@billingServiceBill,@encService,@idGenerate"})
 public class EGovTest {
 	@BeforeClass
 	public static void before() {
